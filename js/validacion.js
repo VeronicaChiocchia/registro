b7a631cb49f1be14460e127b6d31c39bc0cbac31
Milagrosgb <milagrosgbeltrame@icloud.com>
2024-08-15 14:17:43 -0300

    Ningún campo puede estar vacío

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,19 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let contraseña = document.getElementById("password1");
+let repetircontraseña = document.getElementById("password2")
+
+let buttonreg = document.getElementById("regBtn");
+buttonreg.addEventListener("click",function(){
+    if (nombre.value === "" || apellido.value === "" || email.value === "" || contraseña.value === "" || repetircontraseña.value ==="") {
+        showAlertError();
+    } else{
+        showAlertSuccess();
+    }
+});
