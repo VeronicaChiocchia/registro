d43d1e4ca6a2dca0edb832b2bbeeea8d0ceca31d
Romina Espinosa <espinosasosaromina@gmail.com>
2024-08-15 14:50:18 -0300

    actualizo cambios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,18 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let password2 = document.getElementById("password2");
+let password1 = document.getElementById("password1");
+
+ 
+if (password1Value !== password2Value) {
+    showAlertError();
+} else {
+    showAlertSuccess();
+}
+
+}
+
+let regBtn = document.getElementById("regBtn")
+regBtn.addEventListener("click", passwordsAreEqual);
