293fa350cee2feabb768be5bed105f793722c1ff
sofiacurbeloo <sofiacurbelo923@gmail.com>
2024-08-15 15:34:13 -0300

    input-validation

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,33 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+let inputArray = document.querySelectorAll("input"); //obtengo los input
+let checkbox = document.getElementById("terminos"); //obtengo el checkbox
+
+
+function checkboxValidation(){ //validar checkbox
+    if(!checkbox.checked){
+        showAlertError();
+    }
+}
+
+function inputValidation(){ //validar campos
+
+    let count = 0;
+    for (input of inputArray){
+        if(input.value==""){
+            count ++;
+        } 
+    }
+    if (count !== 0){
+        showAlertError();
+    }
+    
+}
+
+regBtn.addEventListener("click", function(){
+    inputValidation();
+    checkboxValidation()
+})
